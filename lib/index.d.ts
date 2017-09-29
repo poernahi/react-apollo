6b60b5e36212e25a29bb86c42eef9ca018f0deda
Hendrik Poernama <poernahi@gmail.com>
2017-09-30 02:04:17 +0700

    Add compiled files for npm consumption

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+import ApolloProvider from './ApolloProvider';
+import graphql from './graphql';
+import { withApollo } from './withApollo';
+import compose from 'redux';
+import { getDataFromTree, renderToStringWithData, walkTree, cleanupApolloState } from './server';
+import gql from 'graphql-tag';
+export { ApolloProvider, graphql, withApollo, compose, getDataFromTree, renderToStringWithData, walkTree, cleanupApolloState, gql };
